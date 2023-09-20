2586ccd1b1e35e0a72a9eca96e889ba9b3168732
magicjuandi <118768572+magicjuandi@users.noreply.github.com>
2023-09-20 08:58:10 -0500

    Practica 2.1

--- src/main/java/conntrollers/Test.java
+++ src/main/java/conntrollers/Test.java
@@ -10,11 +10,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
-@WebServlet(value="/test")
+// Indicamos que esta clase es un servlet y configuramos la URL a la que responderá.
+@WebServlet(value = "/test")
 public class Test extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        // Establecemos el tipo de contenido de la respuesta como HTML.
         resp.setContentType("text/html");
+
+        // Obtenemos información sobre la solicitud HTTP.
         String metodoHttp = req.getMethod();
         String requestUri = req.getRequestURI();
         String requestUrl = req.getRequestURL().toString();
@@ -25,17 +28,23 @@ public class Test extends HttpServlet {
         int port = req.getLocalPort();
         String scheme = req.getScheme();
         String host = req.getHeader("host");
+
+        // Construimos URLs personalizadas a partir de la información obtenida.
         String url = scheme + "://" + host + contexPath + servletPath;
         String url2 = scheme + "://" + ip + ":" + port + contexPath + servletPath;
+
         try (PrintWriter out = resp.getWriter()) {
+            // Comenzamos a generar la página HTML.
             out.println("<!DOCTYPE html>");
             out.println("<html>");
-            out.println(" <head>");
-            out.println(" <meta charset=\"UTF-8\">");
-            out.println(" <title>Cabeceras HTTP Request</title>");
-            out.println(" </head>");
-            out.println(" <body>");
-            out.println(" <h1>Cabeceras HTTP Request!</h1>");
+            out.println("<head>");
+            out.println("<meta charset=\"UTF-8\">");
+            out.println("<title>Cabeceras HTTP Request</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Cabeceras HTTP Request!</h1>");
+
+            // Creamos una lista desordenada para mostrar la información de la solicitud.
             out.println("<ul>");
             out.println("<li>metodo http: " + metodoHttp + "</li>");
             out.println("<li>request uri: " + requestUri + "</li>");
@@ -49,15 +58,19 @@ public class Test extends HttpServlet {
             out.println("<li>host: " + host + "</li>");
             out.println("<li>url: " + url + "</li>");
             out.println("<li>url2: " + url2 + "</li>");
+
+            // Enumeramos todas las cabeceras HTTP de la solicitud y las mostramos en la lista.
             Enumeration<String> headerNames = req.getHeaderNames();
             while (headerNames.hasMoreElements()) {
                 String cabecera = headerNames.nextElement();
-                out.println("<li>"+ cabecera + ": " + req.getHeader(cabecera) + "</li>");
+                out.println("<li>" + cabecera + ": " + req.getHeader(cabecera) + "</li>");
             }
+
+            // Cerramos la lista desordenada y completamos la página HTML.
             out.println("</ul>");
-            out.println(" </body>");
+            out.println("</body>");
             out.println("</html>");
         }
     }
-
 }
+
